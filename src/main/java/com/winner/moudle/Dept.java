c72d51f39b4338305631d5eab56adc3f032dd6ee
zhaoxianglinXL <15736535817@163.com>
2018-11-27 14:45:24 +0800

    第一次提交

---
+++ src/main/java/com/winner/moudle/Dept.java
@@ -0,0 +1,40 @@
+package com.winner.moudle;
+
+import java.io.Serializable;
+
+public class Dept implements Serializable{// entity --orm--- db_table
+	private Long 	deptno; // 主键
+	private String 	dname; // 部门名称
+	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+	
+	public Long getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(Long deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getDb_source() {
+		return db_source;
+	}
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
+	}
+	public Dept(Long deptno, String dname, String db_source) {
+		super();
+		this.deptno = deptno;
+		this.dname = dname;
+		this.db_source = db_source;
+	}
+	public Dept() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
