054f58ce25f3622988758fa37a08a42b13c33b42
zhaoxianglinXL <15736535817@163.com>
2018-11-28 10:56:13 +0800

    微服务提供者8001自测通过

--- src/main/java/com/winner/springcloud/model/DeptInfo.java
+++ src/main/java/com/winner/springcloud/model/DeptInfo.java
@@ -1,8 +1,8 @@
-package com.winner.moudle;
+package com.winner.springcloud.model;
 
 import java.io.Serializable;
 
-public class Dept implements Serializable{// entity --orm--- db_table
+public class DeptInfo implements Serializable{// entity --orm--- db_table
 	private Long 	deptno; // 主键
 	private String 	dname; // 部门名称
 	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
@@ -25,13 +25,13 @@ public class Dept implements Serializable{// entity --orm--- db_table
 	public void setDb_source(String db_source) {
 		this.db_source = db_source;
 	}
-	public Dept(Long deptno, String dname, String db_source) {
+	public DeptInfo(Long deptno, String dname, String db_source) {
 		super();
 		this.deptno = deptno;
 		this.dname = dname;
 		this.db_source = db_source;
 	}
-	public Dept() {
+	public DeptInfo() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
